28e9c751f96c4988404746c9430096693e6c13ba
rjluvgtr <109062851+rjluvgtr@users.noreply.github.com>
2022-11-23 21:44:57 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -2,6 +2,6 @@ import React from 'react';
 export default class Footer extends React.Component{
     render() {
         return <div className='footer'>
-            Copyright © 2021 All rights reserved. Design|Code: Divakar Reddy Varugu</div>
+           </div>
     }
 }
